f4f48d32de1e2742e5a84b6db386efadf8caa582
cgcel <cgc.elvin@outlook.com>
2023-09-03 00:02:05 +0800

    add: run local backup command.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,40 @@
-import { Notice, Plugin } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import { join } from 'path';
 import * as path from 'path';
 import { copy } from 'fs-extra';
 import { mkdir } from 'fs';
 
 
-export default class AutoBackupOnClosePlugin extends Plugin {
+interface LocalBackupPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: LocalBackupPluginSettings = {
+	mySetting: 'default'
+}
+
+export default class LocalBackupPlugin extends Plugin {
+	settings: LocalBackupPluginSettings;
+
 	async onload() {
 		// console.log('loading plugin')
 		// this.app.workspace.on('window-close', await this.backupRepository.bind(this));
-		await this.backupRepository();
+		await this.backupVaultAsync();
+
+		// Run local backup command
+		this.addCommand({
+			id: 'run-local-backup',
+			name: 'Run local backup',
+			callback: () => {
+				this.backupVaultAsync();
+			}
+		});
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
-	async backupRepository() {
+	async backupVaultAsync() {
 		try {
 			const vaultName = this.app.vault.getName();
 			const currentDate = new Date().toISOString().split('T')[0];
@@ -20,7 +42,7 @@ export default class AutoBackupOnClosePlugin extends Plugin {
 			const vaultPath = (this.app.vault.adapter as any).basePath;
 			const parentDir = path.dirname(vaultPath);
 			const backupFolderPath = join(parentDir, backupFolderName);
-			console.log(backupFolderPath);
+			// console.log(backupFolderPath);
 
 			mkdir(backupFolderPath, { recursive: true }, (err) => {
 				if (err) {
@@ -30,9 +52,18 @@ export default class AutoBackupOnClosePlugin extends Plugin {
 				}
 			});
 			// const copy = require("fs-extra")
-			await copy(vaultPath, backupFolderPath);
+			await copy(vaultPath, backupFolderPath); // copy vault to target path
 
-			new Notice(`Repository backup created: ${backupFolderName}`);
+			new Notice(`Repository backup created: ${backupFolderPath}`);
+		} catch (error) {
+			new Notice(`Failed to create repository backup: ${error}`);
+			console.log(error);
+		}
+	}
+
+	async archiveVaultAsync() {
+		try {
+			// ...
 		} catch (error) {
 			new Notice(`Failed to create repository backup: ${error}`);
 			console.log(error);
@@ -40,6 +71,40 @@ export default class AutoBackupOnClosePlugin extends Plugin {
 	}
 
 	async onunload() {
-		console.log('Backup plugin unloaded');
+		console.log('Local Backup unloaded');
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: LocalBackupPlugin;
+
+	constructor(app: App, plugin: LocalBackupPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
 	}
 }
