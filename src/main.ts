8381800c2d036aa5bc8e94de292ec37553800baa
cgcel <cgc.elvin@outlook.com>
2023-11-07 12:03:29 +0800

    update: mv func to utils, add version to data.json

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { Notice, Plugin } from "obsidian";
 import { join } from "path";
-import * as path from "path";
-import * as fs from "fs-extra";
-import AdmZip from "adm-zip";
 import { LocalBackupSettingTab } from "./settings";
 import {
-	getDatePlaceholdersForISO,
 	replaceDatePlaceholdersWithValues,
+	getDefaultPath,
+	getDefaultName,
+	createZipByArchiver,
+	createZipByAdmZip,
+	deleteBackupsByLifeCycle,
+	deletePerDayBackups
 } from "./utils";
-import { exec } from "child_process";
 
 interface LocalBackupPluginSettings {
+	versionValue: string;
 	startupBackupStatus: boolean;
 	lifecycleValue: string;
 	backupsPerDayValue: string;
@@ -25,6 +27,7 @@ interface LocalBackupPluginSettings {
 }
 
 const DEFAULT_SETTINGS: LocalBackupPluginSettings = {
+	versionValue: "",
 	startupBackupStatus: false,
 	lifecycleValue: "3",
 	backupsPerDayValue: "3",
@@ -34,7 +37,7 @@ const DEFAULT_SETTINGS: LocalBackupPluginSettings = {
 	intervalBackupStatus: false,
 	backupFrequencyValue: "10",
 	callingArchiverStatus: false,
-	archiverTypeValue: "",
+	archiverTypeValue: "sevenZip",
 	archiverPathValue: ""
 };
 
@@ -44,6 +47,17 @@ export default class LocalBackupPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+
+		// startup notice
+		if (this.manifest.version === "0.1.0"){
+			try {
+				if (this.settings.versionValue === ""){
+					new Notice("Please recofig `Local Backup` after upgrading to 0.1.0!", 10000);
+				}
+			} catch (error) {
+				new Notice("Please recofig `Local Backup` after upgrading to 0.1.0!", 10000);	
+			}
+		}
 
 		// this.app.workspace.on('window-close', await this.backupRepository.bind(this));
 
@@ -165,6 +179,8 @@ export default class LocalBackupPlugin extends Plugin {
 	}
 
 	async saveSettings() {
+		// set current version in data.json
+		this.settings.versionValue = this.manifest.version;
 		await this.saveData(this.settings);
 	}
 
@@ -193,6 +209,7 @@ export default class LocalBackupPlugin extends Plugin {
 	 * Restore default settings
 	 */
 	async restoreDefault() {
+		this.settings.versionValue = DEFAULT_SETTINGS.versionValue;
 		this.settings.startupBackupStatus = DEFAULT_SETTINGS.startupBackupStatus;
 		this.settings.lifecycleValue = DEFAULT_SETTINGS.lifecycleValue;
 		this.settings.backupsPerDayValue = DEFAULT_SETTINGS.backupsPerDayValue;
@@ -206,218 +223,4 @@ export default class LocalBackupPlugin extends Plugin {
 		this.settings.archiverPathValue = DEFAULT_SETTINGS.archiverPathValue;
 		await this.saveSettings();
 	}
-}
-
-/**
- * Get path of current vault
- * @returns
- */
-function getDefaultPath(): string {
-	const defaultPath = path.dirname((this.app.vault.adapter as any).basePath);
-	// this.settings.savePathSetting = defaultPath
-	return defaultPath;
-}
-
-/**
- * Get default backup name
- * @returns
- */
-function getDefaultName(): string {
-	const vaultName = this.app.vault.getName();
-	const defaultDatePlaceholders = getDatePlaceholdersForISO(true);
-	return `${vaultName}-Backup-${defaultDatePlaceholders}`;
-}
-
-/**
- * Delete backups by lifecycleSetting
- * @param winSavePath
- * @param unixSavePath
- * @param fileNameFormat 
- * @param lifecycle 
- * @returns 
- */
-function deleteBackupsByLifeCycle(
-	winSavePath: string,
-	unixSavePath: string,
-	fileNameFormat: string,
-	lifecycle: string,
-) {
-	console.log("Run deleteBackupsByLifeCycle");
-
-	const os = require("os");
-	const platform = os.platform();
-	let savePathSetting = "";
-
-	if (platform === "win32") {
-		savePathSetting = winSavePath;
-	} else if (platform === "linux" || platform === "darwin") {
-		savePathSetting = unixSavePath;
-	}
-
-	const currentDate = new Date();
-
-	// calculate the target date
-	if (parseInt(lifecycle) !== 0) {
-		currentDate.setDate(currentDate.getDate() - parseInt(lifecycle));
-	}
-
-	fs.readdir(savePathSetting, (err, files) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		// lifecycleSetting
-		if (parseInt(lifecycle) !== 0) {
-			files.forEach((file) => {
-				const filePath = path.join(savePathSetting, file);
-				const stats = fs.statSync(filePath);
-				const fileNameRegex = generateRegexFromCustomPattern(fileNameFormat);
-				const matchFileName = file.match(fileNameRegex);
-
-				if (stats.isFile() && matchFileName !== null) {
-					const parseTime = stats.mtime;
-					const createDate = new Date(parseTime.getFullYear(), parseTime.getMonth(), parseTime.getDate());
-
-					if (createDate < currentDate) {
-						fs.remove(filePath);
-						console.log(`Backup removed by deleteBackupsByLifeCycle: ${filePath}`);
-					}
-				}
-			});
-		}
-	});
-}
-
-/**
- * Delete backups by backupsPerDayValue
- * @param winSavePath
- * @param unixSavePath 
- * @param fileNameFormat 
- * @param backupsPerDay 
- */
-function deletePerDayBackups(
-	winSavePath: string,
-	unixSavePath: string,
-	fileNameFormat: string,
-	backupsPerDay: string
-) {
-	console.log("Run deletePerDayBackups");
-
-	const os = require("os");
-	const platform = os.platform();
-	let savePathSetting = "";
-
-	if (platform === "win32") {
-		savePathSetting = winSavePath;
-	} else if (platform === "linux" || platform === "darwin") {
-		savePathSetting = unixSavePath;
-	}
-
-	fs.readdir(savePathSetting, (err, files) => {
-		if (err) {
-			console.error(err);
-			return;
-		}
-
-		const currentDate = new Date();
-		currentDate.setHours(0, 0, 0, 0);
-		const fileNameRegex = generateRegexFromCustomPattern(fileNameFormat);
-
-		const backupFiles = files.filter((file) => {
-			const filePath = path.join(savePathSetting, file);
-			const stats = fs.statSync(filePath);
-			const matchFileName = file.match(fileNameRegex);
-
-			return stats.isFile() && matchFileName !== null;
-		});
-
-		const todayBackupFiles = backupFiles.filter((file) => {
-			const filePath = path.join(savePathSetting, file);
-			const stats = fs.statSync(filePath);
-			const parseTime = stats.mtime;
-			const createDate = new Date(parseTime.getFullYear(), parseTime.getMonth(), parseTime.getDate());
-
-			return createDate.getTime() === currentDate.getTime();
-		});
-
-		if (todayBackupFiles.length > parseInt(backupsPerDay)) {
-			const filesToDelete = todayBackupFiles.slice(0, todayBackupFiles.length - parseInt(backupsPerDay));
-
-			filesToDelete.forEach((file) => {
-				const filePath = path.join(savePathSetting, file);
-				fs.remove(filePath, (err) => {
-					if (err) {
-						console.error(`Failed to remove backup file: ${filePath}`, err);
-					} else {
-						console.log(`Backup removed by deletePerDayBackups: ${filePath}`);
-					}
-				});
-			});
-		}
-	});
-}
-
-/**
- * Generate regex from custom pattern,
- * @param customPattern 
- * @returns 
- */
-function generateRegexFromCustomPattern(customPattern: string): RegExp {
-	// Replace placeholders like %Y, %m, etc. with corresponding regex patterns
-	const regexPattern = customPattern
-		.replace(/%Y/g, '\\d{4}') // Year
-		.replace(/%m/g, '\\d{2}') // Month
-		.replace(/%d/g, '\\d{2}') // Day
-		.replace(/%H/g, '\\d{2}') // Hour
-		.replace(/%M/g, '\\d{2}') // Minute
-		.replace(/%S/g, '\\d{2}'); // Second
-
-	// Create a regular expression to match the custom pattern
-	return new RegExp(regexPattern);
-}
-
-/**
- * Create zip file by adm-zip
- * @param vaultPath 
- * @param backupZipPath 
- */
-function createZipByAdmZip(vaultPath: string, backupZipPath: string) {
-	// const AdmZip = require("adm-zip");
-	const zip = new AdmZip();
-	zip.addLocalFolder(vaultPath);
-	zip.writeZip(backupZipPath);
-}
-
-/**
- * Create zip file by external archiver
- * @param archiverType 
- * @param archiverPath 
- * @param vaultPath 
- * @param backupZipPath 
- * @returns 
- */
-async function createZipByArchiver(archiverType: string, archiverPath: string, vaultPath: string, backupZipPath: string) {
-
-	switch (archiverType) {
-		case "sevenZip":
-			const promise = new Promise<void>((resolve, reject) => {
-				const command = `"${archiverPath}" a "${backupZipPath}" "${vaultPath}"`;
-		
-				exec(command, (error, stdout, stderr) => {
-					if (error) {
-						console.error("Failed to create zip file by 7-Zip:", error);
-						reject(error);
-					} else {
-						console.log("Zip file created by 7-Zip successfully.");
-						resolve();
-					}
-				});
-			});
-			return promise;
-
-		default:
-			break;
-	}
-	
 }
